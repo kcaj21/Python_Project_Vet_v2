2126d3d23d8bd3206b5364f1687c7e2e2d2932e4
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-06-12 16:30:41 +0100

    added check in dates to db

--- db/vet_database.sql
+++ db/vet_database.sql
@@ -14,13 +14,15 @@ CREATE TABLE animals (
     contact_details VARCHAR(13),
     date_of_birth DATE,
     treatment_notes VARCHAR(255),
+    check_in_date DATE,
+    check_out_date DATE,
     vet_id INT NOT NULL REFERENCES vets(id) ON DELETE CASCADE
 );
- 
+
 INSERT INTO vets (name, description) VALUES ('Dr Ink', 'More likely to anaesthetise themselves before the animal');
 
 
-INSERT INTO animals (name, type, contact_details, date_of_birth, treatment_notes, vet_id) VALUES ('Barney', 'Dinosaur', '+447123456789', '1992-04-06', 'Prosthetic tail insertion', 1);
+INSERT INTO animals (name, type, contact_details, date_of_birth, treatment_notes, check_in_date, check_out_date, vet_id) VALUES ('Barney', 'Dinosaur', '+447123456789', '1992-04-06', 'Prosthetic tail insertion', '2023-04-06', '2023-05-07', 1);
 
 
 -- UPDATE animals SET contact_details = 'none' WHERE name = 'Barney';
