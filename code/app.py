e15d2ed4c2ecd981ac9d2a3b91c2116552ca86dc
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-14 14:32:26 +0100

    route to ignore favicon request added

--- code/app.py
+++ code/app.py
@@ -10,6 +10,11 @@ app.register_blueprint(animals_blueprint)
 def home():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 4999))
+    app.run(host='0.0.0.0', port=port)
 
