dec22250b07c3e7efec86c2caaecd8169e351f7c
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-14 13:35:59 +0100

    upd requirements

--- code/app.py
+++ code/app.py
@@ -13,7 +13,3 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
